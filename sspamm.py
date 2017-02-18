cd1033e4cc385e538a6c6075d71166c9f32a1bf5
Sami-Pekka Hallikas <semi@hallikas.com>
2017-02-19 00:27:19 +0200

    Sorry, I will make small steps, so it will be easier to follow

--- sspamm.py
+++ sspamm.py
@@ -10,4 +10,15 @@ __version__ = "4.0-devel"
 
 import sys
 
+def main():
+	return
+
+if __name__ == "__main__":
+
+	if not sys.argv[1:]:
+		main()
+	else:
+		print """We need help here?"""
+		sys.exit(1)
+
 sys.exit(0)

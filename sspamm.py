d22def073e002e74c7056f5c55ecf22cf5eec841
Sami-Pekka Hallikas <semi@hallikas.com>
2017-02-19 00:24:20 +0200

    init

---
+++ sspamm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""Semi's SPAM Milter
+"""
+__author__ = "Sami-Pekka Hallikas <semi@hallikas.com>"
+__email__ = "semi@hallikas.com"
+__date__ = "19 Feb 2017"
+__version__ = "4.0-devel"
+
+import sys
+
+sys.exit(0)

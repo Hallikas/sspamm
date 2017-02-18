fc34c290156f6e3e589291724ac65137209f20e8
Sami-Pekka Hallikas <semi@hallikas.com>
2017-02-19 00:39:47 +0200

    More basic stuff, main runtime and functions

--- sspamm.py
+++ sspamm.py
@@ -11,24 +11,55 @@ __version__ = "4.0-devel"
 import sys
 import os
 import locale
+import time
 
 from socket import gethostname
 
+##############################################################################
+### Global variables / Configuration
+conf = {}
 
+conf["runtime"] = {
+	"starttime":	0,
+	"endtime":	0,
+	"hostname":	None,
+	"bindir":	None,
+}
+
+##############################################################################
+### Configuration
+
+##############################################################################
+### Main Functions
 def main():
-	return
+	global conf
+
+	time.sleep(1)
+	cleanquit()
+
+def cleanquit():
+	global conf
+
+	conf["runtime"]["endtime"] = "%.0f" % time.time()
+	print "runtime was %.0f seconds" % (int(conf["runtime"]["endtime"])-int(conf["runtime"]["starttime"]))
+
+	sys.stdout.flush()
+	time.sleep(1)
+	os._exit(0)
 
 if __name__ == "__main__":
+	conf["runtime"]["starttime"] = "%.0f" % time.time()
+	conf["runtime"]["startdir"] = os.getcwd()
 
 	os.nice(5)
 	locale.setlocale(locale.LC_CTYPE, 'en_US.UTF-8')
 
 	try:
-		hostname = gethostname()[0:gethostname().index('.')]
+		conf["runtime"]["hostname"] = gethostname()[0:gethostname().index('.')]
 	except:
-		hostname = gethostname()
-	print hostname
+		conf["runtime"]["hostname"] = gethostname()
 
+	conf["runtime"]["bindir"] = sys.argv[0][0:sys.argv[0].rfind("/")]
 	if not sys.argv[1:]:
 		main()
 	else:
